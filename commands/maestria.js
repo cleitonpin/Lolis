71c500ee8c5cc81f17b2eb5896668886e89b95d6
Lucas8x <lucas.poring99@gmail.com>
2020-02-03 15:28:02 -0300

    Update maestria.js and c.js

--- commands/maestria.js
+++ commands/maestria.js
@@ -2,12 +2,22 @@ const Discord = require('discord.js')
 const kayn = require('./kayn')
 const { IDtoName } = require('./commonFunctions')
 
+const masteryImagesURL = {
+    1: "https://imgur.com/9pGhB9z.png",
+    2: "https://imgur.com/UHNjYTw.png",
+    3: "https://imgur.com/b8crQMG.png",
+    4: "https://imgur.com/rgfn9xM.png",
+    5: "https://imgur.com/gtS1Do0.png",
+    6: "https://imgur.com/pPWUtnv.png",
+    7: "https://imgur.com/6UZ5G2P.png"
+}
+
 exports.run = async (client, message, args) => {
-    if(args[0] && args[1]==1) {
+    if(args[0] && args[1] == 1) {
         //pegar o id do jogador
         kayn.kaynObject.Summoner.by.name(`${args[0]}`)
         .callback(function(err, summoner) {
-            console.log("\n\nID do jogador: "+summoner['id']+"\n\n")
+            console.log(`\n\nID do jogador: ${summoner['id']}\n\n`)
             
             //pegar info das maestrias
             kayn.kaynObject.ChampionMastery.list(summoner['id'])
@@ -15,27 +25,20 @@ exports.run = async (client, message, args) => {
                 console.log(ChampionMastery[0])
                 //pegar nome dos champs
                 
+                let masteryLevel = ChampionMastery[0]['championLevel']
+                let masteryImgURL = masteryImagesURL[masteryLevel]
+                let adquiriuBauHextech = ChampionMastery[2]['chestGranted'] ? 'Sim' : 'Não'
 
-                if(ChampionMastery[0]['championLevel']==7) masteryImgURL = "https://imgur.com/6UZ5G2P.png"
-                else if(ChampionMastery[0]['championLevel']==6) masteryImgURL = "https://imgur.com/pPWUtnv.png"
-                else if(ChampionMastery[0]['championLevel']==5) masteryImgURL = "https://imgur.com/gtS1Do0.png"
-                else if(ChampionMastery[0]['championLevel']==4) masteryImgURL = "https://imgur.com/rgfn9xM.png"
-                else if(ChampionMastery[0]['championLevel']==3) masteryImgURL = "https://imgur.com/b8crQMG.png"
-                else if(ChampionMastery[0]['championLevel']==2) masteryImgURL = "https://imgur.com/UHNjYTw.png"
-                else if(ChampionMastery[0]['championLevel']==1) masteryImgURL = "https://imgur.com/9pGhB9z.png"
-
-                if(ChampionMastery[2]['chestGranted']== true) var SimorNot = 'Sim' 
-                if(ChampionMastery[2]['chestGranted']== false) var SimorNot = 'Não' 
                 //var calculo = Math.round(ChampionMastery[0]['lastPlayTime']/60000000000)
                 const embed = new Discord.RichEmbed()
                 .setColor('#170B3B')
                 .setTitle(`Nome do invocador: ***${summoner['name']}***`, '\u200b')
-                .addField('**Campeão**', IDtoName(ChampionMastery[0]['championId']),true)
-                .addField('**Nível de maestria**', ChampionMastery[0]['championLevel'],true)
+                .addField('**Campeão**', IDtoName(ChampionMastery[0]['championId']), true)
+                .addField('**Nível de maestria**', masteryLevel, true)
                 .addField('**Pontos**', ChampionMastery[0]['championPoints'],true)
                 //.addField('**Duração da útima partida**', `${calculo.toFixed(2)} minutos`,true)
-                .addField('**Fragmentos de maestria**', ChampionMastery[0]['tokensEarned'],true)
-                .addField('**Já conseguiu báu hextech nessa season?**', SimorNot, true)
+                .addField('**Fragmentos de maestria**', ChampionMastery[0]['tokensEarned'], true)
+                .addField('**Já conseguiu báu hextech nessa season?**', adquiriuBauHextech, true)
                 .setImage(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${IDtoName(ChampionMastery[0]['championId'])}_0.jpg`)
                 .setThumbnail(masteryImgURL)
                 message.channel.send(embed)
@@ -43,13 +46,11 @@ exports.run = async (client, message, args) => {
         })// fim id do jogador
     }
 
-//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-    else if(args[0] && args[1]==2) {
+    else if(args[0] && args[1] == 2) {
         //pegar o id do jogador
         kayn.kaynObject.Summoner.by.name(`${args[0]}`)
         .callback(function(err, summoner) {
-            console.log("\n\nID do jogador: "+summoner['id']+"\n\n")
+            console.log(`\n\nID do jogador: ${summoner['id']}\n\n`)
             
             //pegar info das maestrias
             kayn.kaynObject.ChampionMastery.list(summoner['id'])
@@ -57,26 +58,20 @@ exports.run = async (client, message, args) => {
                 console.log(ChampionMastery[1])
                 //pegar nome dos champs
                 
-                if(ChampionMastery[1]['championLevel']==7) masteryImgURL = "https://imgur.com/6UZ5G2P.png"
-                else if(ChampionMastery[1]['championLevel']==6) masteryImgURL = "https://imgur.com/pPWUtnv.png"
-                else if(ChampionMastery[1]['championLevel']==5) masteryImgURL = "https://imgur.com/gtS1Do0.png"
-                else if(ChampionMastery[1]['championLevel']==4) masteryImgURL = "https://imgur.com/rgfn9xM.png"
-                else if(ChampionMastery[1]['championLevel']==3) masteryImgURL = "https://imgur.com/b8crQMG.png"
-                else if(ChampionMastery[1]['championLevel']==2) masteryImgURL = "https://imgur.com/UHNjYTw.png"
-                else if(ChampionMastery[1]['championLevel']==1) masteryImgURL = "https://imgur.com/9pGhB9z.png"
-
-                if(ChampionMastery[2]['chestGranted']== true) var SimorNot = 'Sim' 
-                if(ChampionMastery[2]['chestGranted']== false) var SimorNot = 'Não' 
+                let masteryLevel = ChampionMastery[1]['championLevel']
+                let masteryImgURL = masteryImagesURL[masteryLevel]
+                let adquiriuBauHextech = ChampionMastery[2]['chestGranted'] ? 'Sim' : 'Não' 
+                
                 //var calculo = Math.round(ChampionMastery[0]['lastPlayTime']/60000000000)
                 const embed = new Discord.RichEmbed()
                 .setColor('#170B3B')
                 .setTitle(`Nome do invocador: ***${summoner['name']}***`, '\u200b')
-                .addField('**Campeão**', IDtoName(ChampionMastery[1]['championId']),true)
-                .addField('**Nível de maestria**', ChampionMastery[1]['championLevel'],true)
-                .addField('**Pontos**', ChampionMastery[1]['championPoints'],true)
+                .addField('**Campeão**', IDtoName(ChampionMastery[1]['championId']), true)
+                .addField('**Nível de maestria**', ChampionMastery[1]['championLevel'], true)
+                .addField('**Pontos**', ChampionMastery[1]['championPoints'], true)
                 //.addField('**Duração da útima partida**', `${calculo.toFixed(2)} minutos`,true)
-                .addField('**Fragmentos de maestria**', ChampionMastery[1]['tokensEarned'],true)
-                .addField('**Já conseguiu báu hextech nessa season?**', SimorNot)
+                .addField('**Fragmentos de maestria**', ChampionMastery[1]['tokensEarned'], true)
+                .addField('**Já conseguiu báu hextech nessa season?**', adquiriuBauHextech)
                 .setImage(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${IDtoName(ChampionMastery[1]['championId'])}_0.jpg`)
                 .setThumbnail(masteryImgURL)
                 message.channel.send(embed)
@@ -84,13 +79,11 @@ exports.run = async (client, message, args) => {
         })// fim id do jogador
     }
 
-//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-    else if(args[0] && args[1]==3) {
+    else if(args[0] && args[1] == 3) {
         //pegar o id do jogador
         kayn.kaynObject.Summoner.by.name(`${args[0]}`)
         .callback(function(err, summoner) {
-            console.log("\n\nID do jogador: "+summoner['id']+"\n\n")
+            console.log(`\n\nID do jogador: ${summoner['id']}\n\n`)
             
             //pegar info das maestrias
             kayn.kaynObject.ChampionMastery.list(summoner['id'])
@@ -98,35 +91,29 @@ exports.run = async (client, message, args) => {
                 console.log(ChampionMastery[2])
                 //pegar nome dos champs
                 
-
-                if(ChampionMastery[2]['championLevel']==7) masteryImgURL = "https://imgur.com/6UZ5G2P.png"
-                else if(ChampionMastery[2]['championLevel']==6) masteryImgURL = "https://imgur.com/pPWUtnv.png"
-                else if(ChampionMastery[2]['championLevel']==5) masteryImgURL = "https://imgur.com/gtS1Do0.png"
-                else if(ChampionMastery[2]['championLevel']==4) masteryImgURL = "https://imgur.com/rgfn9xM.png"
-                else if(ChampionMastery[2]['championLevel']==3) masteryImgURL = "https://imgur.com/b8crQMG.png"
-                else if(ChampionMastery[2]['championLevel']==2) masteryImgURL = "https://imgur.com/UHNjYTw.png"
-                else if(ChampionMastery[2]['championLevel']==1) masteryImgURL = "https://imgur.com/9pGhB9z.png"
-
-                if(ChampionMastery[2]['chestGranted']== true) var SimorNot = 'Sim' 
-                if(ChampionMastery[2]['chestGranted']== false) var SimorNot = 'Não'    
-                var calculo = Math.round(ChampionMastery[0]['lastPlayTime']/60000000000)        
+                let masteryLevel = ChampionMastery[2]['championLevel']
+                let masteryImgURL = masteryImagesURL[masteryLevel]
+                let adquiriuBauHextech = ChampionMastery[2]['chestGranted'] ? 'Sim' : 'Não'   
+                
+                //var calculo = Math.round(ChampionMastery[0]['lastPlayTime']/60000000000)        
                 const embed = new Discord.RichEmbed()
                 .setColor('#170B3B')
                 .setTitle(`Nome do invocador: ***${summoner['name']}***`, '\u200b')
-                .addField('**Campeão**', IDtoName(ChampionMastery[2]['championId']),true)
-                .addField('**Nível de maestria**', ChampionMastery[2]['championLevel'],true)
-                .addField('**Pontos**', ChampionMastery[2]['championPoints'],true)
-                //.addField('**Duração da útima partida**', `${calculo.toFixed(2)} minutos`,true)
-                .addField('**Fragmentos de maestria**', ChampionMastery[2]['tokensEarned'],true)
+                .addField('**Campeão**', IDtoName(ChampionMastery[2]['championId']), true)
+                .addField('**Nível de maestria**', masteryLevel, true)
+                .addField('**Pontos**', ChampionMastery[2]['championPoints'], true)
+                //.addField('**Duração da útima partida**', `${calculo.toFixed(2)} minutos`, true)
+                .addField('**Fragmentos de maestria**', ChampionMastery[2]['tokensEarned'], true)
+                .addField('**Já conseguiu báu hextech nessa season?**', adquiriuBauHextech)
                 .setImage(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${IDtoName(ChampionMastery[2]['championId'])}_0.jpg`)          
-                .addField('**Já conseguiu báu hextech nessa season?**', SimorNot)
+                
                 .setThumbnail(masteryImgURL)
                 message.channel.send(embed)
             })//fim info das maestrias
         })// fim id do jogador
     }
-    else{
+
+    else {
         message.channel.send('**Digite o nome do invocador e a numeração de 1-3**')
     }
-
 }
