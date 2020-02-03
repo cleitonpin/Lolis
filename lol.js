8974b7bb9a88569305a74c6e331eda84b68d1c70
Lucas8x <lucas.poring99@gmail.com>
2020-02-03 14:46:03 -0300

    Nada

--- lol.js
+++ lol.js
@@ -40,46 +40,47 @@ function generateXp() {
 
 client.on('raw', async dados  => {
     if(dados.t !== "PRESENCE_UPDATE") return
-
-    if(dados.t === "PRESENCE_UPDATE" && client.guilds.get('575815357609148426').members.get(dados.d.user.id)) {
-
+    else if(dados.t === "PRESENCE_UPDATE" && client.guilds.get('575815357609148426').members.get(dados.d.user.id)) {
         //console.log(dados.d)
-
         let serv = client.guilds.get('575815357609148426')
         let membro = serv.members.get(dados.d.user.id)
-        let lol = serv.roles.get('662333273263046667'),
-            apex = serv.roles.get('662699156560936981'),
-            dev = serv.roles.get('661743359735496705'),
-            tft = serv.roles.get('664556186930118656'),
-            js = serv.roles.get('665018735631007754')
-            
-        if(dados.d.game == null || dados.d.game.name == null) return
         
+        let roles = {
+            lol: serv.roles.get('662333273263046667'),
+            apex: serv.roles.get('662699156560936981'),
+            dev: serv.roles.get('661743359735496705'),
+            tft: serv.roles.get('664556186930118656'),
+            js: serv.roles.get('665018735631007754')
+        }
+
+        if(dados.d.game == null || dados.d.game.name == null) return
+
         if (dados.d.game.details.search('.js') > 0) {
-            if(membro.roles.has(js)) return
-            membro.addRole(js)
+            if(!membro.roles.has(roles.js)) {
+                rmembro.addRole(roles.js)
+            }
         }
         console.log(dados.d.game.details.search('.js'))
 
-        
-
-
         if(dados.d.game.name == 'Visual Studio Code') {
-            if(membro.roles.has(dev)) return
-            membro.addRole(dev)
+            if(!membro.roles.has(roles.dev)) {
+                membro.addRole(roles.dev)
+            }
         }
         else if(dados.d.game.name == 'League of Legends') {
             if(dados.d.game.details == 'Teamfight Tactics (Normal)' || dados.d.game.details == 'Teamfight Tactics (Ranqueadas)') {
-                if(membro.roles.has(tft)) return
-                membro.addRole(tft)
-
+                if(!membro.roles.has(roles.tft)) {
+                    membro.addRole(roles.tft)
+                }
             }
-            if(membro.roles.has(lol)) return
-            membro.addRole(lol)
+            if(!membro.roles.has(roles.lol)) {
+                membro.addRole(roles.lol)
+            }
         }
         else if (dados.d.game.name == 'Apex Legends') {
-            if(membro.roles.has(apex)) return
-            membro.addRole(apex)
+            if(!membro.roles.has(roles.apex)) {
+                membro.addRole(roles.apex)
+            }
         }
     }
 })
@@ -129,16 +130,14 @@ client.on("message", async message => {
         const attachment = new Attachment('https://i.imgur.com/w3duR07.png')
         message.channel.send(attachment)
     }
-
-    /*if (message.author.id == '400734254365016068' || message.author.id == '611995739559559223'){
+    /*else if (message.author.id == '400734254365016068' || message.author.id == '611995739559559223'){
         return message.channel.send('Cala boca gustavo')
     }*/
-    if (message.content === 'not stonks') return message.channel.send('https://tenor.com/view/not-stonks-profit-down-sad-frown-arms-crossed-gif-15684535')
+    else if (message.content === 'not stonks') return message.channel.send('https://tenor.com/view/not-stonks-profit-down-sad-frown-arms-crossed-gif-15684535')
     else if(message.content === 'stonks') return message.channel.send('https://tenor.com/view/stonks-noice-glitch-gif-15021121')
     else if (message.content === 'genio') return message.channel.send(client.emojis.get("589690527742558209").toString())
     else if (message.content ==='kappa') return message.channel.send(client.emojis.get("592516025334104086").toString())
     else if(message.content ==='nosa') return message.channel.send(client.emojis.get("598994179464364076").toString())
-
     /*if(message.author.id == '445003940128358401') {
         var att = new Attachment('https://cdn.discordapp.com/attachments/575815357609148428/661022608577527809/5ab2dd770513f.png')
         message.channel.send(att)
@@ -154,7 +153,8 @@ client.on("message", async message => {
         message.reply('Sem comando de música aqui')
         .then(d_message => {
             d_message.delete(1000)
-            console.log(`Deleted message from ${message.author.username}`)})
+            console.log(`Deleted message from ${message.author.username}`)
+        })
     }
     
     try {
